3eb3c21d45e7dd0cb6a1379a8dc1c565f6eb9dda
TyrionJYQ <1003518719@qq.com>
2019-04-28 09:36:59 +0800

    logo component

--- blog_front/src/base/header/header.js
+++ blog_front/src/base/header/header.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { Menu } from "antd";
 import { Layout, Button, Row, Col, Avatar, Popconfirm } from "antd";
+import Logo from '../logo/logo'
 import { pageHeader, userKeyNameInStorage } from "../../config/pageConfig";
 import Http from '../../assets/js/api'
 import "./header.css";
@@ -13,11 +14,14 @@ class MainHeader extends Component {
     super();
     let user = localStorage.getItem(userKeyNameInStorage)
     this.state = {
+      visible: false,
       avatorName: (user ? user[0] : ''),
       theme: 'light'
     };
   }
-
+  setVisible(isShow) {
+    this.setState({visible: isShow})
+  }
   logout() {
     console.log(this.props)
     let bizData = {
@@ -46,12 +50,12 @@ class MainHeader extends Component {
       );
     return (
       <Row>
-        <Header style={{backgroundColor: '#fff'}}>
+        <Header style={{ backgroundColor: '#fff' }}>
           <Col span={3}>
-            <div className="logo" />
+            <Logo />
           </Col>
           <Col span={18} >
-          <Menu
+            <Menu
               theme={this.state.theme}
               mode="horizontal"
               defaultSelectedKeys={["1"]}
@@ -60,8 +64,8 @@ class MainHeader extends Component {
               <Menu.Item key="1">
                 <Link to="/main/home">首页</Link>
               </Menu.Item>
-             
-             
+
+
               <Menu.Item key="2">
                 <Link to="/main/archive">分类</Link>
               </Menu.Item>
@@ -71,8 +75,14 @@ class MainHeader extends Component {
             </Menu>
           </Col>
 
-          <Col span={3} style={{textAlign: 'right'}}>
-            <Popconfirm title="退出登录?" onConfirm={() => this.logout()} onCancel={() => null} okText="Yes" cancelText="No">
+          <Col span={3} style={{ textAlign: 'right' }}>
+            <Popconfirm title="退出登录?"
+              visible={this.state.visible}
+              onConfirm={() => this.logout()}
+              onCancel={() => this.setVisible(false)}
+              onMouseEnter={e => this.setVisible(true)}
+              onClick = {() => this.setVisible(!this.state.visible)}
+              okText="是" cancelText="否">
               {user}
             </Popconfirm>,
           </Col>
---
+++ blog_front/src/base/logo/logo.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import './logo.css';
+class Logo extends Component {
+  render() {
+    return <h3 className="logo">Tyrion Blog</h3>
+  }
+}
+export default Logo;
