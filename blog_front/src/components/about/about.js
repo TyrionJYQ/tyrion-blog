d2bf82325a98fa13495d7cde2d69494bbd891248
TyrionJYQ <1003518719@qq.com>
2020-01-20 22:51:16 +0800

    add git && csdn blog link to aboutPae

--- blog_front/src/components/about/about.js
+++ blog_front/src/components/about/about.js
@@ -3,7 +3,10 @@ import React, { Component } from 'react';
 class About extends Component {
   render() {
     return (
-      <p>我是关于页面</p>
+      <ul>
+        <li><a href="https://github.com/TyrionJYQ/" target="blank">github</a></li>
+        <li><a href="https://blog.csdn.net/TyrionJ" target="blank">csdn</a></li>
+      </ul>
     )
   }
 }
